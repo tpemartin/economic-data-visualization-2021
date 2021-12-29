89a6528a0a2dd5af3c36a2d1c1bab47fc4a43df2
tpemartin <tpemartin@gmail.com>
2021-12-29 12:47:44 +0800

    update

--- support/color.R
+++ support/color.R
@@ -6,3 +6,8 @@ x_in <- seq(0, 1, length.out = length(colors))
 l_interp <- stats::approxfun(x_in, lab_in[, 1])
 u_interp <- stats::approxfun(x_in, lab_in[, 2])
 v_interp <- stats::approxfun(x_in, lab_in[, 3])
+
+colors <- 
+farver::decode_colour(colors, alpha = TRUE, to = "hsl", 
+  na_value = "transparent")
+colors
